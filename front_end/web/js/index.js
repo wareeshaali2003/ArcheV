66f21cec94459bcc2c8006147f1d05d248b1cc63
wareeshaali2003 <wareeshaali2003@gmail.com>
2024-03-04 12:30:42 +0500

    updated

--- front_end/web/js/index.js
+++ front_end/web/js/index.js
@@ -2,5 +2,5 @@ async function open_folder_dialog() {
     let  folderPath = await pywebview.api.open_folder_dialog();
     console.log(folderPath); 
     console.log(typeof(folderPath));
-    document.getElementById("folderInput").value = await pywebview.api.open_folder_dialog();
+    document.getElementById("folderPathInput").value = await pywebview.api.open_folder_dialog();
 }
--- main.py
+++ main.py
@@ -5,4 +5,4 @@ from front_end.expose_functs import *
 # Drivers code
 if __name__ == "__main__":
     windows["main"] = webview.create_window(title="ArcheV", url="front_end/web/index.html", resizable=False,width=1570,height=900)
-    webview.start(expose, [windows["main"]], debug=True)
+    webview.start(expose, [windows["main"]])
